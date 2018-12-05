9354852f47d6775f946aaf818b9851caf615cbee
unknown <ashishchide1101@gmail.com>
2018-12-06 00:34:51 +0530

    navbar collapse

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  collapsed = true;
+     toggleCollapsed(): void {
+       this.collapsed = !this.collapsed;
+     }
   constructor() { }
 
   ngOnInit() {
